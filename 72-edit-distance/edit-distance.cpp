7ebcf60251d8654658b9d18c513d46871382f9aa
Nilardri Pramanick <nilardripramanick2006@gmail.com>
2026-09-19 18:12:34 +0530

    Time: 4 ms (81.75%) | Memory: 10.6 MB (90.34%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -12,16 +12,39 @@ public:
         if (s1[i - 1] == s2[j - 1])
             return dp[i][j] = f(i - 1, j - 1, s1, s2, dp);
 
-return dp[i][j] = 1 + min({
-            f(i - 1, j, s1, s2, dp),     // delete
-            f(i, j - 1, s1, s2, dp),     // insert
-            f(i - 1, j - 1, s1, s2, dp)  // replace
-        });
+        return dp[i][j] = 1 + min({
+                                  f(i - 1, j, s1, s2, dp),    // delete
+                                  f(i, j - 1, s1, s2, dp),    // insert
+                                  f(i - 1, j - 1, s1, s2, dp) // replace
+                              });
     }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return f(n, m, word1, word2, dp);
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        vector<int>prev(m+1,0);
+        vector<int>curr(m+1,0);
+        // for (int i = 0; i <= n; i++)
+        //     dp[i][0] = i;
+        // for (int j = 1; j <= m; j++)
+        //     dp[0][j] = j;
+        for (int j = 0; j <= m; j++)
+            prev[j] = j;
+
+        for (int i = 1; i <= n; i++) {
+            curr[0] = i;
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1])
+                     curr[j] = prev[j-1];
+
+                else curr[j] = 1 + min({
+                                          prev[j],    // delete
+                                          curr[j-1],    // insert
+                                          prev[j-1] // replace
+                                      });
+            }
+            prev = curr;
+        }
+        return prev[m];
     }
 };
